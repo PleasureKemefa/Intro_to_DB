206ef90ead2a60063b30e1e3faf98b4cfd3e9fc0
PleasureKemefa <80639745+PleasureKemefa@users.noreply.github.com>
2025-06-29 07:27:28 -0700

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,14 @@
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
+
+USE alx_book_store;
+-- SQL script to describe the Books table in the alx_book_store database.
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS -- <-- THIS LINE WAS CHANGED
+WHERE
+    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
